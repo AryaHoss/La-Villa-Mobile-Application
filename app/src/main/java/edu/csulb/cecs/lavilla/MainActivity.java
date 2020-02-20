5e0f48a4f0dcab266e400b8972f8eba6dc9f250f
Javier Sanchez <javiii03@dhcp-39-13-15.dhcpint.csulb.edu>
2020-02-19 18:09:13 -0800

    Initial commit

---
+++ app/src/main/java/edu/csulb/cecs/lavilla/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csulb.cecs.lavilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
