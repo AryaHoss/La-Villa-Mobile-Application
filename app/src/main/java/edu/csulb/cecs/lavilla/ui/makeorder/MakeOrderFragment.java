2c10b59911a675934e3f90e3642a212c42233bdd
chris88herr <chris88herrera@gmail.com>
2020-02-28 14:37:45 -0800

    updateded makeorderViewModel to store locations Updated buttons in MakeOrder main fragemets to reflext order type selections

--- app/src/main/java/edu/csulb/cecs/lavilla/ui/makeorder/MakeOrderFragment.java
+++ app/src/main/java/edu/csulb/cecs/lavilla/ui/makeorder/MakeOrderFragment.java
@@ -1,5 +1,6 @@
 package edu.csulb.cecs.lavilla.ui.makeorder;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -16,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import edu.csulb.cecs.lavilla.R;
+import edu.csulb.cecs.lavilla.ui.makeorder.Data.Order;
 
 public class MakeOrderFragment extends Fragment {
 
@@ -35,19 +37,25 @@ public class MakeOrderFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MakeOrderViewModel.class);
         // TODO: Use the ViewModel
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mViewModel = new  ViewModelProvider(getActivity()).get(MakeOrderViewModel.class);
+
         Button deliveryButton =  view.findViewById(R.id.delivery_button);
         deliveryButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mViewModel.setOrderType(Order.OrderType.DELIVERY); // set the type of order selected
+
                 NavController navController = Navigation.findNavController(getActivity(), R.id.make_order_navHost);
                 navController.navigate(R.id.action_makeOrderFragment_to_makeOrderLocations);
+
             }
         });
 
@@ -55,6 +63,9 @@ public class MakeOrderFragment extends Fragment {
         pickupButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mViewModel.setOrderType(Order.OrderType.PICKUP); // set the tyoe of order selected
+
                 NavController navController = Navigation.findNavController(getActivity(), R.id.make_order_navHost);
                 navController.navigate(R.id.action_makeOrderFragment_to_makeOrderLocations);
             }
--- app/src/main/java/edu/csulb/cecs/lavilla/ui/makeorder/MakeOrderViewModel.java
+++ app/src/main/java/edu/csulb/cecs/lavilla/ui/makeorder/MakeOrderViewModel.java
@@ -3,13 +3,14 @@ package edu.csulb.cecs.lavilla.ui.makeorder;
 import androidx.lifecycle.ViewModel;
 import edu.csulb.cecs.lavilla.ui.makeorder.Data.*;
 
-import java.util.ArrayList;
-
 public class MakeOrderViewModel extends ViewModel {
     // TODO: Implement the ViewModel
     private Order order;
+    private Locations locations;
 
-    public MakeOrderViewModel() {
+
+    public MakeOrderViewModel(Locations locations) {
+        this.locations = locations;
         order = new Order();
     }
 
