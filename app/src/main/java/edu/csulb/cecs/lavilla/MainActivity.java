63cd15ac18ea89d3568d934203e5fcd0776be747
Tyler Ton <tylerton.05@gmail.com>
2020-02-27 23:19:01 -0800

    Test Read and Write to Firebase

--- app/src/main/java/edu/csulb/cecs/lavilla/MainActivity.java
+++ app/src/main/java/edu/csulb/cecs/lavilla/MainActivity.java
@@ -1,14 +1,49 @@
 package edu.csulb.cecs.lavilla;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    public TextView rest_phone, rest_hours, rest_address,rest_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rest_phone = findViewById(R.id.rest_phone);
+        rest_hours = findViewById(R.id.rest_hours);
+        rest_address = findViewById(R.id.rest_address);
+        rest_name = findViewById(R.id.rest_name);
+
+        RestaurantModel restaurant = new RestaurantModel("1250 Bellflower Blvd\nLong Beach, CA 90840", "Mon - Fri: 10:00 to 22:00\nWeekend: Closed", "(562) 985-4111");
+        database.child("restaurants").child("001").setValue(restaurant);
+
+        DatabaseReference ref = database.child("restaurants").child("001");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                RestaurantModel rest = dataSnapshot.getValue(RestaurantModel.class);
+                rest_phone.setText(rest.getPhoneNumber());
+                rest_hours.setText(rest.getHours());
+                rest_address.setText(rest.getAddress());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/csulb/cecs/lavilla/RestaurantModel.java
@@ -0,0 +1,50 @@
+package edu.csulb.cecs.lavilla;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class RestaurantModel {
+    private String address;
+    private String hours;
+    private String phoneNumber;
+
+    public RestaurantModel() {
+
+    }
+
+    public RestaurantModel(String address, String hours, String phoneNumber) {
+        this.address = address;
+        this.hours = hours;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
