be25e911257cbd181a20805f27ff3b58d059f58f
chris88herr <chris88herrera@gmail.com>
2020-03-11 11:49:33 -0700

    implemented logic for Submited Order fragment

--- app/src/main/java/edu/csulb/cecs/lavilla/MakeOrderSubmited.java
+++ app/src/main/java/edu/csulb/cecs/lavilla/MakeOrderSubmited.java
@@ -5,13 +5,22 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import edu.csulb.cecs.lavilla.ui.makeorder.MakeOrderViewModel;
+import edu.csulb.cecs.lavilla.ui.makeorder.adapters.CartAdapter;
 
 
 public class MakeOrderSubmited extends Fragment {
+
+    ListView detailsListView;
+    MakeOrderViewModel mViewModel;
+    CartAdapter cartAdapter;
 
 
     public MakeOrderSubmited() {
@@ -32,7 +41,16 @@ public class MakeOrderSubmited extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_make_order_submited, container, false);
+        View view = inflater.inflate(R.layout.fragment_make_order_submited, container, false);
+
+        mViewModel = new ViewModelProvider(getActivity()).get(MakeOrderViewModel.class);
+        detailsListView =(ListView) view.findViewById(R.id.order_details_listview);
+        cartAdapter = new CartAdapter(getContext(), R.layout.cart_adapter_view_layout, mViewModel.getOrder().getPickedItems());
+        detailsListView.setAdapter(cartAdapter);
+
+
+        return view;
+
     }
 
     public void onButtonPressed(Uri uri) {
