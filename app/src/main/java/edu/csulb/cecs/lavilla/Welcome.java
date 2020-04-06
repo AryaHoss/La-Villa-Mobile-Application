ed52023ba67552072e61de6b865506bff36af6f3
Brandon Gibson <brandongibson93@yahoo.com>
2020-04-06 14:52:31 -0700

    Added Welcome View

---
+++ app/src/main/java/edu/csulb/cecs/lavilla/Welcome.java
@@ -0,0 +1,33 @@
+package edu.csulb.cecs.lavilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Welcome extends AppCompatActivity {
+
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //After 3 seconds switches to login activity
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                Intent intent = new Intent(Welcome.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+        }, 3000);
+
+    }
+
+
+}
