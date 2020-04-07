80486e317a5af3701db6326089ea6bf921c2aa27
Brandon Gibson <brandongibson93@yahoo.com>
2020-04-06 18:35:04 -0700

    Added Login Activity

---
+++ app/src/main/java/edu/csulb/cecs/lavilla/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.csulb.cecs.lavilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
