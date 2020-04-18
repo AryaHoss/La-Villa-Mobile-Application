ec96260e04825231013f6edd5594d0f2c1af3300
Brandon Gibson <brandongibson93@yahoo.com>
2020-04-17 17:11:37 -0700

    Added UserViewModel class

--- app/src/main/java/edu/csulb/cecs/lavilla/ui/makeorder/UserViewModel.java
+++ app/src/main/java/edu/csulb/cecs/lavilla/ui/makeorder/UserViewModel.java
@@ -1,23 +1,144 @@
 package edu.csulb.cecs.lavilla.ui.makeorder;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.csulb.cecs.lavilla.User;
+import edu.csulb.cecs.lavilla.UserAccountEdit;
 
 public class UserViewModel extends ViewModel {
     private DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase firebaseDatabase;
     User user;
+    private String uid;
 
 
     UserViewModel(){
         user = new User();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
     }
+
+    public void getfname (String newName){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
+        ref.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String userFirstName = dataSnapshot.child("fname").getValue(String.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                databaseError.getMessage();
+
+
+            }
+        });
+
+    }
+
+    public void getlname (String newName){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
+        ref.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String userLastName = dataSnapshot.child("lname").getValue(String.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                databaseError.getMessage();
+
+
+            }
+        });
+
+    }
+
+    public void getemail (String newName){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
+        ref.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String userEmail = dataSnapshot.child("email").getValue(String.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                databaseError.getMessage();
+
+
+            }
+        });
+
+    }
+
+    public void changefName(String newName){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
+        ref.child(uid).child("fname").setValue(newName);
+
+    }
+
+    public void changelName(String newName){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
+        ref.child(uid).child("lname").setValue(newName);
+
+    }
+
+    public void changeEmail(String newName){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = firebaseDatabase.getReference("Users");
+        ref.child(uid).child("email").setValue(newName);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.updateEmail(newName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+
+                        }
+                    }
+                });
+
+    }
+
+
 
 
 }
